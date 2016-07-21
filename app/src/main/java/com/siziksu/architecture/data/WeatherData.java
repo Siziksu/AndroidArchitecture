91ef95002128cc3cf9b0fd0c5829bb9d82bb6376
Esteban Latre <covert_operations@hotmail.com>
2016-07-21 10:15:18 +0200

    refactor

--- app/src/main/java/com/siziksu/architecture/data/WeatherData.java
+++ app/src/main/java/com/siziksu/architecture/data/WeatherData.java
@@ -1,5 +1,6 @@
 package com.siziksu.architecture.data;
 
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.google.gson.Gson;
@@ -12,8 +13,8 @@ import com.siziksu.architecture.data.client.weather.WeatherClient;
  */
 public final class WeatherData {
 
-    private static final String WEATHER = "weather";
     private static final String WEATHER_CACHE = "weather_cache";
+    private static final String WEATHER_CACHE_TIME = "weather_cache_time";
     private static final long WEATHER_EXPIRY_TIME = 10000;
 
     /**
@@ -25,26 +26,26 @@ public final class WeatherData {
      */
     public OpenWeather getWeather(String city) {
         OpenWeather openWeather;
-        String cache = Preferences.getInstance().getString(WEATHER, null);
-        long cache_time = Preferences.getInstance().getLong(WEATHER_CACHE, System.currentTimeMillis());
-        if (cacheExists(cache) && cacheIsNotExpired(cache_time)) {
+        String cache = Preferences.getInstance().getString(WEATHER_CACHE, null);
+        long cacheTime = Preferences.getInstance().getLong(WEATHER_CACHE_TIME, System.currentTimeMillis());
+        if (cacheExists(cache) && cacheIsNotExpired(WEATHER_EXPIRY_TIME, cacheTime)) {
             openWeather = new Gson().fromJson(cache, OpenWeather.class);
         } else {
             openWeather = new WeatherClient().getWeather(city);
             if (openWeather != null) {
                 cache = new Gson().toJson(openWeather);
-                Preferences.getInstance().setString(WEATHER, cache);
-                Preferences.getInstance().setLong(WEATHER_CACHE, System.currentTimeMillis());
+                Preferences.getInstance().setString(WEATHER_CACHE, cache);
+                Preferences.getInstance().setLong(WEATHER_CACHE_TIME, System.currentTimeMillis());
             }
         }
         return openWeather;
     }
 
-    private boolean cacheExists(String cache) {
+    private boolean cacheExists(@NonNull String cache) {
         return !TextUtils.isEmpty(cache);
     }
 
-    private boolean cacheIsNotExpired(long cache_time) {
-        return cache_time + WEATHER_EXPIRY_TIME >= System.currentTimeMillis();
+    private boolean cacheIsNotExpired(long expireTime, long cacheTime) {
+        return cacheTime + expireTime >= System.currentTimeMillis();
     }
 }
